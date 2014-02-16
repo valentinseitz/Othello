f999fb007e0c6751c49f2a69350cf3219cacebe8
valentinseitz <valentinseitz@hotmail.fr>
2014-02-16 22:43:31 +0100

    Extension des possibilités d'un jeton et d'un plateau

--- src/com/ema/othelloVE/Jeton.java
+++ src/com/ema/othelloVE/Jeton.java
@@ -1,11 +1,25 @@
 package com.ema.othelloVE;
 
 public class Jeton {
-	
+
 	public static final byte BLANC = 1;
 	public static final byte NOIR = 2;
 	public static final byte VIDE = 0;
-	public static final byte VERT = 3;
 
-	
+	public static byte jetonAdversaire(byte jeton) {
+		byte jetonAdversaire;
+		switch (jeton) {
+		case BLANC:
+			jetonAdversaire = NOIR;
+			break;
+		case NOIR:
+			jetonAdversaire = BLANC;
+			break;
+		default:
+			jetonAdversaire = VIDE;
+			break;
+		}
+		return jetonAdversaire;
+	}
+
 }
