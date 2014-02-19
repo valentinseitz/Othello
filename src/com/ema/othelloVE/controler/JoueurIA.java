080b1bcfb1162b9fb75735719268fb00eb315355
valentinseitz <valentinseitz@hotmail.fr>
2014-02-20 00:54:19 +0100

    Implémentation de la recherche alphaBeta

--- src/com/ema/othelloVE/controler/JoueurIA.java
+++ src/com/ema/othelloVE/controler/JoueurIA.java
@@ -10,6 +10,7 @@ import com.ema.othelloVE.model.Coup;
 import com.ema.othelloVE.model.Jeton;
 import com.ema.othelloVE.model.Plateau;
 
+import android.graphics.Point;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -32,18 +33,8 @@ public class JoueurIA extends Joueur {
 		controlJeu = control;
 	}
 
-	private class JoueurExpert extends AsyncTask<Void, Void, Coup> { // classe
-																		// permettant
-																		// de
-																		// lancer
-																		// une
-																		// tâche
-																		// en
-																		// fond
-																		// (pour
-																		// les
-																		// calculs
-																		// longs)
+	private class JoueurExpert extends AsyncTask<Void, Void, Coup> {
+		// Classe permettant de lancer une tâche en fond(pour les calculs longs)
 		protected void onPreExecute() {
 		}
 
@@ -55,27 +46,30 @@ public class JoueurIA extends Joueur {
 		protected void onPostExecute(Coup coupExpert) {
 			p = coupExpert;
 			sendResult(p);
-
 		}
 	}
 
 	private void sendResult(Coup coup) { // retour du coup calculé au contrôleur
-		controlJeu.publishEvent(coup.getLigne(), coup.getColonne(), this);
+		controlJeu.publishEvent(coup.getX(), coup.getY(), this);
 	}
 
-	public void calculCoup() { // calcul du coup à jouer en fonction de la force du joueurIA
+	public void calculCoup() { // calcul du coup à jouer en fonction de la force
+								// du joueurIA
 		p = null;
 		switch (force) {
 		case DEBUTANT:
-			p = calculCoupDebutant(); // fonction courte qui peut s'exécuter suite à la demande du contrôleur
+			p = calculCoupDebutant(); // fonction courte qui peut s'exécuter
+										// suite à la demande du contrôleur
 			sendResult(p);
 			break;
 		case MOYEN:
-			p = calculCoupMoyen(); // fonction courte qui peut s'exécuter suite à la demande du contrôleur
+			p = calculCoupMoyen(); // fonction courte qui peut s'exécuter suite
+									// à la demande du contrôleur
 			sendResult(p);
 			break;
 		case EXPERT:
-			new JoueurExpert().execute(); // fonction chronophage : à lancer en tâche de fond
+			new JoueurExpert().execute(); // fonction chronophage : à lancer en
+											// tâche de fond
 			break;
 
 		}
@@ -84,25 +78,22 @@ public class JoueurIA extends Joueur {
 
 	private Coup calculCoupDebutant() { // retourne un coup possible choisi
 										// aléatoirement
-		List<HeuristiqueCoord> possibles = new ArrayList<HeuristiqueCoord>();
-		HeuristiqueCoord coord;
+		List<Point> possibles = new ArrayList<Point>();
 		Random rand = new Random();
 		int index;
 		Coup coup;
 		for (int i = 0; i < Plateau.TAILLE; i++) {
 			for (int j = 0; j < Plateau.TAILLE; j++) {
-				if (ControleurPlateau.nbRetournements(plateau, i, j, this.couleur, false) > 0) {
-					coord = new HeuristiqueCoord();
-					coord.setX(i);
-					coord.setY(j);
-					possibles.add(coord);
+				if (ControleurPlateau.nbRetournements(plateau, i, j,
+						this.couleur, false) > 0) {
+					possibles.add(new Point(i, j));
 				}
 			}
 		}
 		if (possibles.size() > 0) {
 			index = rand.nextInt(possibles.size());
-			coup = new Coup(possibles.get(index).getX(), possibles.get(index)
-					.getY(), this.couleur);
+			coup = new Coup(possibles.get(index).x, possibles.get(index).y,
+					this.couleur);
 		} else {
 			coup = null;
 		}
@@ -115,8 +106,7 @@ public class JoueurIA extends Joueur {
 										// retournements
 										// sur arbre de recherche développé à 1
 										// niveau
-		List<HeuristiqueCoord> possibles = new ArrayList<HeuristiqueCoord>();
-		HeuristiqueCoord coord;
+		List<Point> possibles = new ArrayList<Point>();
 		int heuristique;
 		int maxHeuristique;
 		Random rand = new Random();
@@ -126,26 +116,22 @@ public class JoueurIA extends Joueur {
 		maxHeuristique = -1;
 		for (int i = 0; i < Plateau.TAILLE; i++) {
 			for (int j = 0; j < Plateau.TAILLE; j++) {
-					heuristique = ControleurPlateau.nbRetournements(
-							plateau, i, j, this.couleur, false);
-					if (heuristique > 0 && heuristique >= maxHeuristique) {
-						if (heuristique > maxHeuristique) {
-							possibles.clear();
-							maxHeuristique = heuristique;
-						}
-						coord = new HeuristiqueCoord();
-						coord.setX(i);
-						coord.setY(j);
-						coord.setHeuristique(heuristique);
-						possibles.add(coord);
+				heuristique = ControleurPlateau.nbRetournements(plateau, i, j,
+						this.couleur, false);
+				if (heuristique > 0 && heuristique >= maxHeuristique) {
+					if (heuristique > maxHeuristique) {
+						possibles.clear();
+						maxHeuristique = heuristique;
 					}
+					possibles.add(new Point(i, j));
+				}
 			}
 		}
 
 		if (possibles.size() > 0) {
 			index = rand.nextInt(possibles.size());
-			coup = new Coup(possibles.get(index).getX(), possibles.get(index)
-					.getY(), this.couleur);
+			coup = new Coup(possibles.get(index).x, possibles.get(index).y,
+					this.couleur);
 		} else {
 			coup = null;
 		}
@@ -154,43 +140,171 @@ public class JoueurIA extends Joueur {
 
 	}
 
-	private Coup calculCoupExpert() { // retourne le meilleur coup
-										// obtenu par recherche Minmax sur arbre
-										// de recherche développé à 2 ou 3
-										// niveaux
-										// A COMPLETER
-		return (new Coup(4, 2, Jeton.NOIR));
+	private Coup calculCoupExpert() {
+		// Retourne le meilleur coup obtenu par recherche Minmax sur arbre de
+		// recherche développé à 2 ou 3 niveaux
+		return alphaBeta(this.plateau, this.couleur, 3);
 
 	}
 
-	private class HeuristiqueCoord {
-		private int heuristique;
-		private int x;
-		private int y;
+	private Coup alphaBeta(Plateau plateau, Jeton joueur, int profondeur) {
+		// Initialisation à -infini pour alpha et +infini pour beta
+		List<Point> possibles;
+		List<Point> meilleursPossibles = new ArrayList<Point>();
+		int heuristique;
+		int maxHeuristique;
+
+		maxHeuristique = Integer.MIN_VALUE;
+		possibles = ControleurPlateau.coupsPossibles(plateau, joueur);
+		for (Point coup : possibles){
+				heuristique = alphaBeta(new Noeud(plateau, coup,
+						joueur, profondeur), Integer.MIN_VALUE,
+						Integer.MAX_VALUE);
+				if (heuristique > 0 && heuristique >= maxHeuristique) {
+					if (heuristique > maxHeuristique) {
+						meilleursPossibles.clear();
+						maxHeuristique = heuristique;
+					}
+					meilleursPossibles.add(coup);
+				}
+		}
+
+		Random rand = new Random();
+		int index;
+		Coup coup;
+		if (meilleursPossibles.size() > 0) {
+			index = rand.nextInt(meilleursPossibles.size());
+			coup = new Coup(meilleursPossibles.get(index).x, meilleursPossibles.get(index).y,
+					this.couleur);
+		} else {
+			coup = null;
+		}
+
+		return coup;
+	}
+
+	private int alphaBeta(Noeud noeud, int alpha, int beta) {
+		//Alpha est toujous inférieur à beta
+		int heuristique;
+		boolean coupe = false; // P à vrai s'il y a une coupure alpha ou beta
+		if (noeud.isFeuille()) {
+			heuristique = noeud.getHeuristique();
+		} else {
+			if (!noeud.isMax()) {
+				heuristique = Integer.MAX_VALUE;// +infini
+				while (!coupe && noeud.aFilsSuivant()) {
+					heuristique = Math.min(heuristique,
+							alphaBeta(noeud.getFilsSuivant(), alpha, beta));
+					if (alpha >= heuristique) {
+						// Coupure beta
+						coupe = true;
+					} else {
+						beta = Math.min(beta, noeud.getHeuristique());
+					}
+				}
+			} else {
+				heuristique = Integer.MIN_VALUE;// -infini
+				while (!coupe && noeud.aFilsSuivant()) {
+					heuristique = Math.max(heuristique,
+							alphaBeta(noeud.getFilsSuivant(), alpha, beta));
+					if (beta <= heuristique) {
+						// Coupure beta
+						coupe = true;
+					} else {
+						alpha = Math.max(alpha, noeud.getHeuristique());
+					}
+				}
+			}
+		}
+		return heuristique;
+	}
+
+	class Noeud {
+
+		boolean max;
+		int profondeur;
+		Jeton joueur;
+		Plateau plateau;
+		List<Point> coupsPossibles;
+		int coupCourant;
+
+		public Noeud(Plateau plateau, Point coup, Jeton joueur, int profondeur) {
+			this(plateau, coup, joueur, profondeur, true);
+		}
+
+		private Noeud(Plateau plateau, Point coup, Jeton joueur,
+				int profondeur, boolean max) {
+			Jeton joueurNoeud;
+			// Niveau max ou pas?
+			this.max = max;
+			this.joueur = joueur;
+			// Quelle profondeur et quel joueur?
+			if (this.max) {
+				this.profondeur = profondeur;
+				joueurNoeud = this.joueur;
+			} else {
+				this.profondeur = profondeur - 1;
+				joueurNoeud = this.joueur.getAdversaire();
+			}
+			// Clone du plateau
+			this.plateau = new Plateau(plateau);
+			// Un coup à jouer?
+			// On joue le coup
+			if (coup != null) {
+				ControleurPlateau.nbRetournements(this.plateau, coup.x, coup.y,
+						joueurNoeud, true);
+			}
+			//
+			// On demande les coups possibles de l'adversaire
+			coupsPossibles = ControleurPlateau.coupsPossibles(plateau,
+					joueurNoeud.getAdversaire());
+			// Pas encore de coup courant
+			coupCourant = -1;
+		}
+
+		public boolean isFeuille() {
+			// La profondeur suivante est 0, on est donc sur la feuille
+			return profondeur == 1 && !this.max;
+		}
+
+		public boolean isMax() {
+			// Noeud de niveau max
+			return this.max;
+		}
 
 		public int getHeuristique() {
-			return heuristique;
+			// Le nombre de jeton du joueur, moins celui de son adversaire
+			return plateau.nombreJetons(joueur)
+					- plateau.nombreJetons(joueur.getAdversaire());
 		}
 
-		public void setHeuristique(int heuristique) {
-			this.heuristique = heuristique;
+		public boolean aFilsSuivant() {
+			// Il y a un autre coup possible
+			return coupCourant + 1 < coupsPossibles.size()
+					|| (coupCourant == -1 && coupsPossibles.size() == 0);
 		}
 
-		public int getX() {
-			return x;
+		public Noeud getFilsSuivant() {
+			Noeud fils;
+			if (aFilsSuivant()) {
+				// On passe sur le coup suivant
+				coupCourant++;
+				if (coupCourant < coupsPossibles.size()) {
+					// On crée un nouveau noeud qui correspond au coup
+					fils = new Noeud(this.plateau,
+							coupsPossibles.get(coupCourant), joueur,
+							this.profondeur, !max);
+				} else {
+					fils = new Noeud(this.plateau, null, joueur, this.profondeur,
+							!max);
+				}
+			} else {
+				// Il n'y a pas de fils suivant
+				fils = null;
+			}
+			return fils;
 		}
 
-		public void setX(int x) {
-			this.x = x;
-		}
-
-		public int getY() {
-			return y;
-		}
-
-		public void setY(int y) {
-			this.y = y;
-		}
 	}
 
 }
